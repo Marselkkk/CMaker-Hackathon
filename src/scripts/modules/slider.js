517e4ea4c46a2d160a6f7bd49e208a760636ec41
Marcelkkk <marsgyl@gmail.com>
2025-03-20 09:54:37 +0300

    upd

--- src/scripts/modules/slider.js
+++ src/scripts/modules/slider.js
@@ -1,5 +1,21 @@
 export function slider() {
+    const portfolioSlider = new Swiper('.portfolio_slider', {
+        slidesPerView: 3,
+        spaceBetween: getRem() * 2.63,
+        freeMode: true,
+    });
+    const help_sliderlider = new Swiper('.help_slider', {
+        slidesPerView: 3,
+        spaceBetween: getRem() * 1.5,
+        freeMode: true,
+    });
 
+    function getRem() {
+        var element = document.querySelector("html");
+        var fontSize = window.getComputedStyle(element).fontSize;
+        var numericFontSize = parseInt(fontSize, 10);
+        return fontSize.slice(0, -2)
+    }
 }
 
 // Для корректной работы необходимо подключить и активировать эту функцию в app.js
